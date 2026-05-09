b5ebe931310a9d40c42f52ecad540bcfc6f4e1d0
Neelaveni <maddulaneelaveni12793@gmail.com>
2026-05-09 22:00:00 +0530

    file updated

--- Arr_problems.py
+++ Arr_problems.py
@@ -62,3 +62,20 @@ class Solution:
             if complement in hashmap:
                 return [hashmap[complement], i]
             hashmap[num] = i
+# Time Complexity : O(n) : We traverse the list once.
+# Space Complexity : O(n) : In the worst case, we might store all n elements in the hashmap.
+# Here, I used a hashmap (dictionary) to store the indices of the numbers we have seen so far. For each number, we calculate its complement (the value needed to reach the target) and check if it exists in the hashmap. If it does, we return the indices of the complement and the current number. If not, we add the current number and its index to the hashmap for future reference.
+
+
+
+# 4.Find the first 2 highest frequency elements in an array
+# Array = [1, 7, 0, 4, 3, 2, 3, 7, 0]
+
+from collections import defaultdict
+nums = [1, 7, 0, 4, 3, 2, 3, 7, 0]
+freq = defaultdict(int)
+for n in nums:
+    freq[n] += 1
+top2 = sorted(freq, key=freq.get, reverse=True)[:2]
+print(top2)
+
